caca63bc04569e7c4c24d5f323667bf5d7ab77fe
Logan Richardson <reviewedge56@gmail.com>
2022-03-09 22:39:09 +0100

    setup database!

---
+++ sample/database_connector.py
@@ -0,0 +1,78 @@
+import sqlite3
+from sqlite3 import Error
+from datetime import datetime
+
+db = r"C:\Users\RICHARDSONLG20\IdeaProjects\ouch\db\pythonsqlite.db"
+
+def create_connection(db_file):
+    # create a database connection to a SQLite database
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def store_spend(conn, spend):
+    sql = ''' INSERT INTO spends(cost, description, category, date)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, spend)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_spend(cost, category, description=None, custom_datetime_object=None):
+    if custom_datetime_object:
+        date_now = custom_datetime_object
+    else:
+        date_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    conn = create_connection(db)
+    with conn:
+        new_spend = (cost, description, category, date_now)
+        spend_id = store_spend(conn, new_spend)
+    conn.close()
+
+    return spend_id
+
+
+def get_all_spend_cost_sum():
+    conn = create_connection(db)
+
+    sql = ''' SELECT SUM(cost)
+              FROM spends; '''
+    cur = conn.cursor()
+    cur.execute(sql)
+
+    cost_sum = "{:.2f}".format(cur.fetchone()[0])
+
+    conn.close()
+
+    return cost_sum
+
+
+def get_cost_sum_in_cat(category):
+    conn = create_connection(db)
+
+    print(category) #delete me
+
+    sql = ''' SELECT SUM(cost)
+              FROM spends
+              WHERE Category==?; '''
+    cur = conn.cursor()
+    cur.execute(sql, (category,))
+
+    cost_sum = "{:.2f}".format(cur.fetchone()[0])
+
+    conn.close()
+
+    return cost_sum
+
+
+if __name__ == '__main__':
+    create_spend(.75, "gf", "small ball of fun")
+    get_all_spend_cost_sum()
+
--- sample/reader.py
+++ sample/reader.py
@@ -1,6 +1,7 @@
 from telethon import TelegramClient, events
 from forex_python.converter import CurrencyRates
 import re
+import database_connector as dbc
 import config
 
 api_id = config.my_api_id
@@ -10,41 +11,100 @@ bot_token = config.my_bot_token
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
 
+def is_float(element):
+    try:
+        float(element)
+        return True
+    except ValueError:
+        return False
+
+
 def track_spend(message):
     # gets rid of leading dollar sign
     message = message.replace("$", "")
+
     delim = message.split()
 
-    try:
-        total = float(delim[0])
-    except ValueError:
+    """
+        accepted inputs:
+            gf 4 e
+            4 e
+            gf 4
+            4
+    """
+
+    currency = "u"
+    category = None
+
+    if len(delim) == 3:
+        category = delim[0]
+        if is_float(delim[1]):
+            total = delim[1]
+        else:
+            return "Invalid input"
+        currency = delim[2]
+
+    elif len(delim) == 2:
+        if is_float(delim[0]):
+            total = delim[0]
+            currency = delim[1]
+        elif is_float(delim[1]):
+            category = delim[0]
+            total = delim[1]
+        else:
+            return "Invalid input"
+    elif (len(delim) == 1) and (is_float(delim[0])):
+        total = delim[0]
+    else:
         return "Invalid input"
 
-    # ignores if parameters are missing and assumes usd spend
-    try:
-        currency = delim[1]
-    except IndexError:
-        currency = "u"
+    total = float(total)
 
+    # converts from Euros
     if (currency == "e") or (currency == "€"):
         c = CurrencyRates(force_decimal=False)
         total = c.convert("EUR", "USD", total)
 
-    # if currency == "g" or "£":
-    #     c = CurrencyRates(force_decimal=False)
-    #     total = c.convert("GBP", "USD", total)
+    # converts from Great British Pounds
+    if (currency == "g") or (currency == "£"):
+        c = CurrencyRates(force_decimal=False)
+        total = c.convert("GBP", "USD", total)
 
-    return "tracking $" + "{:.2f}".format(total) + " USD"
+    total = round(total, 2)
+
+    print("created spend " + str(dbc.create_spend(total, category)))
+    return "tracking $" + "{:.2f}".format(total) + " USD in category: " + str(category)
 
 
+def send_sum(message):
+    delim = message.split()
+
+    if len(delim) <= 1:
+        output = "All-time total: $" + str(dbc.get_all_spend_cost_sum())
+    else:
+        category = delim[1]
+        output = "All-time total in \"" + category + "\" is $" + str(dbc.get_cost_sum_in_cat(category))
+
+    return output
+
+
+# read new messages
 @bot.on(events.NewMessage)
 async def my_event_handler(event):
+    event.raw_text = event.raw_text.lower()
+
+    # tracks spend if message contains numbers
     if re.search(r'\d', event.raw_text):
         await event.reply(track_spend(event.raw_text))
+
+    if "tot" in event.raw_text:
+        await event.reply(send_sum(event.raw_text))
 
     if "undo" in event.raw_text:
         print("this is where you undo the last db transaction")
 
 
-bot.start()
-bot.run_until_disconnected()
+# run the bot
+if __name__ == '__main__':
+    bot.start()
+    bot.run_until_disconnected()
